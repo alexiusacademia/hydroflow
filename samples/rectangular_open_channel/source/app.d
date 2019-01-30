aa0809a289e6f5b1ebf5a1d2429733cf9d76513a
Alexius Academia <alexius.academia@gmail.com>
2019-01-31 05:49:18 +0800

    Added sample implementation.

---
+++ samples/rectangular_open_channel/source/app.d
@@ -0,0 +1,31 @@
+import std.stdio;
+import std.datetime;
+import std.math;
+
+import hydroflow;
+
+void main()
+{
+  const auto t1 = Clock.currTime;
+
+  RectangularOpenChannel roc = new RectangularOpenChannel();
+
+  roc.setUnknown = roc.Unknown.WATER_DEPTH;
+
+  // Set the given values
+  roc.setBedSlope = 0.001;
+  roc.setDischarge = 1;
+  roc.setBaseWidth = 1;
+  roc.setManningRoughness = 0.015;
+
+  const success = roc.solve();
+
+  const auto t2 = Clock.currTime;
+
+  if (success) {
+    writeln("Water depth = ", roc.getWaterDepth);
+  } else {
+    writeln(roc.errorMessage);
+  }
+}
+
