2acad82a892b53ebab20b691afd58d675b214cce
Alexius Academia <alexius.academia@gmail.com>
2019-02-03 06:30:02 +0800

    Updated constructors, getters and setters.

--- source/libs/irregular_section_open_channel.d
+++ source/libs/irregular_section_open_channel.d
@@ -19,27 +19,63 @@ class IrregularSectionOpenChannel : OpenChannel
     private float waterElevation;
     private double trialDischarge;
 
+    /+++++++++++++++++++++++++++++++++++++++++++++++ 
+    +                  Setters                     +
+    +++++++++++++++++++++++++++++++++++++++++++++++/
+    /**
+    *   Set the points with an array of Point object.
+    */
+    void setPoints(Point[] pts)
+    {
+        points = pts;
+    }
+
+    /**
+    *   Sets the water elevation.
+    */
+    void setWaterElevation(float we)
+    {
+        waterElevation = we;
+    }
+
+    /+++++++++++++++++++++++++++++++++++++++++++++++ 
+    +                  Getters                     +
+    +++++++++++++++++++++++++++++++++++++++++++++++/
+
+    /** Returns the list of points */
+    Point[] getPoints()
+    {
+        return points;
+    }
+
+    /** Returns the adjusted points. */
+    Point[] getNewPoints()
+    {
+        return newPoints;
+    }
+
+    float getMaxWaterElevation()
+    {
+        return maxWaterElevation;
+    }
+
+    float getWaterElevation()
+    {
+        return waterElevation;
+    }
+
+    /+++++++++++++++++++++++++++++++++++++++++++++++ 
+    +                Constructors                  +
+    +++++++++++++++++++++++++++++++++++++++++++++++/
     /// Empty constructor
     this()
     {
         unknown = Unknown.DISCHARGE;
     }
 
-    /+++++++++++++++++++++++++++++++++++++++++++++++ 
-    +                  Setters                     +
-    +++++++++++++++++++++++++++++++++++++++++++++++/
-    void setPoints(Point[] pts)
+    this(Unknown u)
     {
-        points = pts;
-    }
-
-    /+++++++++++++++++++++++++++++++++++++++++++++++ 
-    +                  Getters                     +
-    +++++++++++++++++++++++++++++++++++++++++++++++/
-    /** Returns the list of points */
-    Point[] getPoints()
-    {
-        return points;
+        unknown = u;
     }
 
     /+++++++++++++++++++++++++++++++++++++++++++++++
@@ -47,9 +83,10 @@ class IrregularSectionOpenChannel : OpenChannel
     +++++++++++++++++++++++++++++++++++++++++++++++/
     /** Add a single point */
     void addPoint(Point p)
-    {   
+    {
         ulong lastIndex = points.length;
         points.length = points.length + 1;
         points[lastIndex] = p;
     }
+
 }
