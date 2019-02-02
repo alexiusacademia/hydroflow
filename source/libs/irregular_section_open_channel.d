559829baca1a00abf296472717fa51d4bd0ea5d9
Alexius Academia <alexius.academia@gmail.com>
2019-02-02 11:55:55 +0800

    Started class for Irregular Section channel.

--- source/libs/irregular_section_open_channel.d
+++ source/libs/irregular_section_open_channel.d
@@ -1,1 +1,31 @@
 module libs.irregular_section_open_channel;
+
+/// Standard modules
+import std.math;
+import std.stdio;
+
+// Custom modules
+import libs.openchannel;
+import libs.utils.point;
+
+class IrregularSectionOpenChannel : OpenChannel
+{
+    /+++++++++++++++++++++++++++++++++++++++++++++++
+    +                 Properties                   +
+    +++++++++++++++++++++++++++++++++++++++++++++++/
+    private Point[] points;
+    private Point[] newPoints;
+    private float maxWaterElevation;
+    private float waterElevation;
+    private double trialDischarge;
+
+    /// Empty constructor
+    this()
+    {
+        unknown = Unknown.DISCHARGE;
+        points = new Point[];
+        newPoints = new Point[];
+    }
+
+
+}
