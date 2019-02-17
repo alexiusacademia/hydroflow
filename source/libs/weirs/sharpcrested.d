eaccba4d118cfb1926f3b3190492711e8e36d175
Alexius Academia <alexius.academia@gmail.com>
2019-02-17 19:14:37 +0800

    Created class for sharp crested weirs.

---
+++ source/libs/weirs/sharpcrested.d
@@ -0,0 +1,21 @@
+/**
+    libs.weirs.sharpcrested module
+    Module for hydraulic analysis of sharp crested weirs used in diversion dams.
+*/
+module libs.weirs.sharpcrested;
+
+/**
+    SharpCrestedWeir class.
+    Contains fields and methods for hydraulic calculations
+    of sharp crested weirs.
+*/
+class SharpCrestedWeir 
+{
+    /**
+        Empty constructor.
+    */
+    this()
+    {
+
+    }
+}
